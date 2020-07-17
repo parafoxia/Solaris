a1b7b1c783cb6e0a77df8af2d6e79cedba968904
parafoxia <parafoxia@carberra.xyz>
2020-07-17 01:11:47 +0100

    Upgraded menu system
    Now has support for pagemaps (dicts of keywords) and load keys.
    All now in a menu util of it's own.

--- s4/utils/menu/selectors.py
+++ s4/utils/menu/selectors.py
@@ -1,50 +1,10 @@
 from asyncio import TimeoutError
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 from s4.utils import chron
 
 
-class Menu:
-    def __init__(self, ctx, start_key, stop_key=None, timeout_key=None):
-        self.ctx = ctx
-        self.bot = ctx.bot
-        self.start_key = start_key
-        self.stop_key = stop_key
-        self.timeout_key = timeout_key
-        self.message = None
-
-    async def start(self, **kwargs):
-        self.message = await self.ctx.send(embed=self.bot.embed.load(self.start_key, ctx=self.ctx, **kwargs))
-
-    async def stop(self, **kwargs):
-        if self.stop_key is not None:
-            await self.switch(self.stop_key, clear_reactions=True, **kwargs)
-        else:
-            await self.message.delete()
-
-    async def timeout(self, **kwargs):
-        if self.timeout_key is not None:
-            length = kwargs.get("length", None)
-            await self.switch(self.timeout_key, clear_reactions=True, length=length, **kwargs)
-        else:
-            await self.stop(**kwargs)
-
-    async def switch(self, key, clear_reactions=False, **kwargs):
-        if clear_reactions:
-            await self.message.clear_reactions()
-
-        await self.message.edit(embed=self.bot.embed.load(key, ctx=self.ctx, **kwargs))
-
-    def __repr__(self):
-        return (
-            f"<Menu start_key={self.start_key!r}"
-            f" stop_key={self.stop_key!r}"
-            f" timeout_key={self.timeout_key!r}"
-            f" message={self.message!r}>"
-        )
-
-
-class Selection:
+class Selector:
     def __init__(self, menu, selection, *, timeout=300.0, auto_exit=True, check=None):
         self.menu = menu
         self.timeout = timeout
@@ -88,31 +48,16 @@ class Selection:
                 return r
 
     def __repr__(self):
-        return f"<Selection timeout={self.timeout!r} auto_exit={self.auto_exit!r} menu={self.menu!r}>"
-
-
-class SelectionMenu(Menu):
-    def __init__(
-        self, ctx, selection, start_key, stop_key=None, timeout_key=None, *, timeout=300.0, auto_exit=True, check=None
-    ):
-        super().__init__(ctx, start_key, stop_key, timeout_key)
-        self.selection = Selection(self, selection, timeout=timeout, auto_exit=auto_exit, check=check)
-
-    async def start(self, **kwargs):
-        await super().start(**kwargs)
-        return await self.selection.response()
-
-    def __repr__(self):
         return (
-            f"<SelectionMenu start_key={self.start_key!r}"
-            f" stop_key={self.stop_key!r}"
-            f" timeout_key={self.timeout_key!r}"
-            f" selection={self.selection!r}"
-            f" message={self.message!r}>"
+            f"<Selector"
+            f" timeout={self.timeout!r}"
+            f" auto_exit={self.auto_exit!r}"
+            f" check={self.check!r}"
+            f" menu={self.menu!r}>"
         )
 
 
-class NumberedSelection(Selection):
+class NumericalSelector(Selector):
     def __init__(self, menu, iterable, *, timeout=300.0, auto_exit=True, check=None):
         super().__init__(menu, ["exit"], timeout=timeout, auto_exit=auto_exit, check=check)
 
@@ -201,57 +146,27 @@ class NumberedSelection(Selection):
             else:
                 return self.pages[self.page][r]
 
-            await self.menu.switch_page()
-            await self.menu.message.remove_reaction(reaction, self.menu.ctx.author)
+            await self.menu.switch(reaction)
             return await self.response()
 
     def __repr__(self):
         return (
-            f"<NumberedSelection timeout={self.timeout!r}"
-            f" auto_exit={self.auto_exit!r}"
+            f"<NumericalSelector"
             f" page={self.page!r}"
             f" max_page={self.max_page!r}"
+            f" timeout={self.timeout!r}"
+            f" auto_exit={self.auto_exit!r}"
+            f" check={self.check!r}"
             f" menu={self.menu!r}>"
         )
 
 
-class NumberedSelectionMenu(Menu):
-    def __init__(
-        self, ctx, iterable, start_key, stop_key=None, timeout_key=None, *, timeout=300.0, auto_exit=True, check=None
-    ):
-        super().__init__(ctx, start_key, stop_key, timeout_key)
-        self.selection = NumberedSelection(self, iterable, timeout=timeout, auto_exit=auto_exit, check=check)
-
-        self._options = {}
-
-    @property
-    def page_fields(self):
-        return [(f"{self.selection.page_info}", f"{self.selection.table}", False)]
-
-    async def start(self, **kwargs):
-        self._options = kwargs
-        await super().start(fields=self.page_fields, **kwargs)
-        return await self.selection.response()
-
-    async def switch_page(self):
-        await self.switch(self.start_key, fields=self.page_fields, **self._options)
-
-    def __repr__(self):
-        return (
-            f"<NumberedSelectionMenu start_key={self.start_key!r}"
-            f" stop_key={self.stop_key!r}"
-            f" timeout_key={self.timeout_key!r}"
-            f" selection={self.selection!r}"
-            f" message={self.message!r}>"
-        )
-
-
-class PageControls(Selection):
-    def __init__(self, menu, pages, *, timeout=300.0, auto_exit=True, check=None):
+class PageControls(Selector):
+    def __init__(self, menu, pagemaps, *, timeout=300.0, auto_exit=True, check=None):
         super().__init__(menu, ["exit"], timeout=timeout, auto_exit=auto_exit, check=check)
 
-        self.pages = pages
-        self.max_page = len(pages)
+        self.pagemaps = pagemaps
+        self.max_page = len(pagemaps)
 
         self._selection = []
         self._last_selection = []
@@ -286,7 +201,7 @@ class PageControls(Selection):
         s = self._base_selection.copy()
         insert_point = 0
 
-        if len(self.pages) > 1:
+        if len(self.pagemaps) > 1:
             if self.page != 0:
                 s.insert(0, "pageback")
                 s.insert(0, "stepback")
@@ -322,31 +237,16 @@ class PageControls(Selection):
             elif r == "stepnext":
                 self.page = self.max_page
 
-            await self.menu.switch(self.pages[self.page], **self.menu._options)
-            await self.menu.message.remove_reaction(reaction, self.menu.ctx.author)
+            await self.menu.switch(reaction)
             return await self.response()
 
     def __repr__(self):
-        return f"<PageControls>"
-
-
-class MultiPageMenu(Menu):
-    def __init__(self, ctx, pages, stop_key=None, timeout_key=None, *, timeout=300.0, auto_exit=True, check=None):
-        super().__init__(ctx, pages[0], stop_key, timeout_key)
-        self.selection = PageControls(self, pages, timeout=timeout, auto_exit=auto_exit, check=check)
-
-        self._options = {}
-
-    async def start(self, **kwargs):
-        self._options = kwargs
-        await super().start(**kwargs)
-        return await self.selection.response()
-
-    def __repr__(self):
         return (
-            f"<MultiPagedMenu start_key={self.start_key!r}"
-            f" stop_key={self.stop_key!r}"
-            f" timeout_key={self.timeout_key!r}"
-            f" selection={self.selection!r}"
-            f" message={self.message!r}>"
+            f"<NumericalSelector"
+            f" page={self.page!r}"
+            f" max_page={self.max_page!r}"
+            f" timeout={self.timeout!r}"
+            f" auto_exit={self.auto_exit!r}"
+            f" check={self.check!r}"
+            f" menu={self.menu!r}>"
         )
