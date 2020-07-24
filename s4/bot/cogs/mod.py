99e1e740c76d3d72efe9489d8a9f4dc38441940b
parafoxia <parafoxia@carberra.xyz>
2020-07-24 20:51:56 +0100

    Added extra mod commands
    Added nickname based commands.
    Fixed problems in the kick and ban commands.

--- s4/bot/cogs/mod.py
+++ s4/bot/cogs/mod.py
@@ -1,9 +1,10 @@
+import re
 import typing as t
 
 import discord
 from discord.ext import commands
 
-from s4.utils import chron
+from s4.utils import chron, converters
 
 
 class Mod(commands.Cog):
@@ -23,7 +24,6 @@ class Mod(commands.Cog):
     ):
         if not targets:
             await ctx.send(self.bot.message.load("no targets"))
-
         else:
             kicks = 0
 
@@ -31,6 +31,7 @@ class Mod(commands.Cog):
                 for target in targets:
                     try:
                         await target.kick(reason=f"{reason} - Actioned by {ctx.author.name}")
+                        kicks += 1
                     except discord.Forbidden:
                         await ctx.send(self.bot.message.load("unable to kick", member=target))
 
@@ -55,17 +56,16 @@ class Mod(commands.Cog):
 
         if not targets:
             await ctx.send(self.bot.message.load("no targets"))
-
         elif not 0 <= delete_message_days <= 7:
             await ctx.send(self.bot.message.load("delete message days outside bounds"))
-
         else:
             bans = 0
 
             async with ctx.channel.typing():
                 for target in targets:
                     try:
-                        await target.kick(reason=f"{reason} - Actioned by {ctx.author.name}")
+                        await target.ban(delete_message_days=delete_message_days, reason=f"{reason} - Actioned by {ctx.author.name}")
+                        bans += 1
                     except discord.Forbidden:
                         await ctx.send(self.bot.message.load("unable to ban", member=target))
 
@@ -74,7 +74,16 @@ class Mod(commands.Cog):
                 else:
                     await ctx.send(self.bot.message.load("ban failed"))
 
-    @commands.command(name="clear")
+    @commands.command(name="unban")
+    @commands.has_permissions(ban_members=True)
+    @commands.bot_has_permissions(send_messages=True, ban_members=True)
+    async def unban_command(
+        self, ctx, targets: commands.Greedy[discord.Member], *, reason: t.Optional[str] = "No reason provided."
+    ):
+        # TODO: Actually write this.
+        await ctx.send("Not implemented.")
+
+    @commands.command(name="clear", aliases=["clr"])
     @commands.has_permissions(manage_messages=True)
     @commands.bot_has_permissions(send_messages=True, manage_messages=True)
     async def clear_command(self, ctx, scan: int, targets: commands.Greedy[discord.Member]):
@@ -83,7 +92,6 @@ class Mod(commands.Cog):
 
         if not 0 < scan <= 100:
             await ctx.send(self.bot.message.load("scan outside bounds"))
-
         else:
             async with ctx.channel.typing():
                 await ctx.message.delete()
@@ -92,17 +100,20 @@ class Mod(commands.Cog):
                     self.bot.message.load("clear success", cleared=len(cleared)), delete_after=5,
                 )
 
-    @commands.command(name="clearchannel")
+    @commands.command(name="clearchannel", aliases=["clrch"])
     @commands.has_permissions(manage_messages=True, manage_channels=True)
     @commands.bot_has_permissions(send_messages=True, manage_channels=True)
     async def clearchannel_command(
         self, ctx, target: discord.TextChannel, *, reason: t.Optional[str] = "No reason provided."
     ):
+        ctx_is_target = ctx.channel == target
+
         async with ctx.channel.typing():
             await target.clone(reason=f"{reason} - Actioned by {ctx.author.name}")
             await target.delete(reason=f"Channel cleared. - Actioned by {ctx.author.name}")
-            # TODO: Set this to the log channel.
-            # await ctx.send(self.bot.message.load("clear channel success"))
+
+            if not ctx_is_target:
+                await ctx.send(self.bot.message.load("clear channel success"))
 
     @commands.command(name="mute")
     @commands.has_permissions(manage_roles=True)
@@ -112,6 +123,61 @@ class Mod(commands.Cog):
     ):
         # TODO: Actually write this.
         await ctx.send("Not implemented.")
+
+    @commands.command(name="setnickname", aliases=["setnick"])
+    @commands.has_permissions(manage_nicknames=True)
+    @commands.bot_has_permissions(send_messages=True, manage_nicknames=True)
+    async def setnickname_command(
+        self, ctx, target: t.Union[discord.Member, converters.SearchedMember, str], *, nickname: str
+    ):
+        target = target or ctx.author
+
+        if len(nickname) > 32:
+            await ctx.send(self.bot.message.load("nickname too long"))
+        elif not isinstance(target, discord.Member):
+            await ctx.send(self.bot.message.load("invalid member"))
+        else:
+            try:
+                await target.edit(nick=nickname)
+                await ctx.send(self.bot.message.load("nickname change success"))
+            except discord.Forbidden:
+                await ctx.send(self.bot.message.load("nickname change failed"))
+
+    @commands.command(name="clearnickname", aliases=["clrnick"])
+    @commands.has_permissions(manage_nicknames=True)
+    @commands.bot_has_permissions(send_messages=True, manage_nicknames=True)
+    async def clearnickname_command(self, ctx, targets: commands.Greedy[discord.Member]):
+        clears = 0
+
+        async with ctx.channel.typing():
+            for target in targets:
+                try:
+                    await target.edit(nick=None)
+                    clears += 1
+                except discord.Forbidden:
+                    await ctx.send(self.bot.message.load("unable to clear nickname"))
+
+            if clears > 0:
+                await ctx.send(self.bot.message.load("nickname clear success", clears=clears))
+            else:
+                await ctx.send(self.bot.message.load("nickname clear failed"))
+
+    @commands.command(name="unhoistnicknames")
+    @commands.has_permissions(manage_nicknames=True)
+    @commands.bot_has_permissions(send_messages=True, manage_nicknames=True)
+    async def unhoistnicknames_command(self, ctx):
+        unhoists = 0
+
+        async with ctx.channel.typing():
+            for member in ctx.guild.members:
+                try:
+                    if (match := re.match("[^A-Za-z]+", member.display_name)) is not None:
+                        await member.edit(nick=member.display_name.replace(match.group(), "", 1))
+                        unhoists += 1
+                except discord.Forbidden:
+                    pass
+
+            await ctx.send(self.bot.message.load("nickname unhoist complete", unhoists=unhoists))
 
 
 def setup(bot):
