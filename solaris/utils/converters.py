d849ce35243792ec131135f89a7c9c6b559f29f5
parafoxia <parafoxia@carberra.xyz>
2020-08-28 23:37:16 +0100

    Beautify

--- solaris/utils/converters.py
+++ solaris/utils/converters.py
@@ -55,7 +55,12 @@ class Command(commands.Converter):
 
 class SearchedMember(commands.Converter):
     async def convert(self, ctx, arg):
-        if (member := discord.utils.get(ctx.guild.members, name=str(Search(arg, [m.display_name for m in ctx.guild.members]).best(min_accuracy=0.75)))) is None:
+        if (
+            member := discord.utils.get(
+                ctx.guild.members,
+                name=str(Search(arg, [m.display_name for m in ctx.guild.members]).best(min_accuracy=0.75)),
+            )
+        ) is None:
             raise commands.BadArgument
         return member
 
